776753a023effd7ac26ee7e3f486ff6cfdb53c9f
tvermilion <vermilionjthomas@gmail.com>
2026-01-02 16:13:21 -0500

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "Eleventy Base Blog v9",
-	url: "https://example.com/",
+	title: "Runner's High Blog",
+	url: "https://runnershigh.pages.dev/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "I am writing about my experiences as a running navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Tom",
+		email: "",
+		url: "https://runnershigh.pages.dev/"
 	}
 }
